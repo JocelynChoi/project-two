346ad2ea6023faa06c7dd79da0f177246ef69304
Jocelyn Choi <choi.jocelyn1000@gmail.com>
2018-05-24 18:40:55 -0400

    front-end

---
+++ server.js
@@ -0,0 +1,129 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Suanmokk54!$",
+  database: "movieplannerdb"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// index.handlebars shows our origin page where you can create your pages.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM movies;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { movies: data });
+  });
+});
+//login.handlebars allows users to login
+app.get("/login", function(req, res) {
+    connection.query("SELECT * FROM movies;", function(err, data) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      res.render("login", { movies: data });
+    });
+  });
+//mypage.handlebars allows users to create their own page
+  app.get("/mypage", function(req, res) {
+    connection.query("SELECT * FROM movies;", function(err, data) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      res.render("mypage", { movies: data });
+    });
+  });
+
+// Create a new travel plan
+app.post("/movies", function(req, res) {
+  connection.query("INSERT INTO movies (movie) VALUES (?)", [req.body.movie], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new plan
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Retrieve all travel plans
+app.get("/movies", function(req, res) {
+  connection.query("SELECT * FROM movies;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.json(data);
+  });
+});
+
+// Update a movie
+app.put("/movies/:id", function(req, res) {
+  connection.query("UPDATE movies SET movie = ? WHERE id = ?", [req.body.movie, req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Delete a movie
+app.delete("/movies/:id", function(req, res) {
+  connection.query("DELETE FROM movies WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
